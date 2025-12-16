9abb557606a130de428feeff034aced85e4befc6
ThomasCastro2005 <118085112+ThomasCastro2005@users.noreply.github.com>
2025-12-16 13:17:43 -0500

    Fix import path for MenuAdmin.css

--- src/components/panelAdministrador/MenuAdmin.jsx
+++ src/components/panelAdministrador/MenuAdmin.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../../styles/panelAdministrador/MenuAdmin.css'
+import '../../src/styles/panelAdministrador/MenuAdmin.css'
 
 export const MenuAdmin = () => {
     return (
